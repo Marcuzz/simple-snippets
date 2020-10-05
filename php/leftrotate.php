bdc6ed37c3e4178a7cbf08ea77d26eab46c18064
Theodoor <17050455+TheodoorP@users.noreply.github.com>
2020-10-05 14:21:53 +0000

    added leftrotate function

---
+++ php/leftrotate.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+function rotateLeft($d, $arr) {
+    for ($i = 0; $i < $d; $i++) {
+        $val = $arr[$i];
+        unset($arr[$i]);
+        $arr[] = $val;
+    }
+
+    return $arr;
+}
